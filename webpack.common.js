112faf88eeef2f359b9ec7bf4f13cb7b065ab146
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-03-11 13:07:14 -0400

    Add additional loaders

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
@@ -10,7 +22,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Webpack Template',
+            title: 'Weather App',
             inject: 'head',
             scriptLoading: 'defer',
         }),
