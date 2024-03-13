924dc7dadaa2954719a11be7a0f1881994e91418
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-03-13 12:28:38 -0400

    Add pubsub topics

--- src/api.js
+++ src/api.js
@@ -1,6 +1,11 @@
+import PubSub from 'pubsub-js';
+
+export const DISPLAY_ERROR = 'display error message';
+export const HIDE_ERROR = 'hide error message';
+
 const API_KEY = '69b56b4aea6d430eb7a222431240303';
 
-export async function getDailyAndHourlyWeatherData(cityName){
+async function getDailyAndHourlyWeatherData(cityName){
     const httpResponse = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}&days=3`, {mode: 'cors'});
     const responseBody = await httpResponse.json();
 
@@ -18,10 +23,15 @@ export async function getWeatherData(cityName) {
 
         const dailyHourlyWeatherResponseBody = await getDailyAndHourlyWeatherData(cityName);
 
+        PubSub.publish(HIDE_ERROR);
+
         return { currWeatherResponseBody, dailyHourlyWeatherResponseBody };
     }catch(err){
         err.message = 'UNABLE TO FIND WEATHER DATA FOR THAT LOCATION';
         console.log(err);
+
+        PubSub.publish(DISPLAY_ERROR);
+
         return err;
     }
 }
