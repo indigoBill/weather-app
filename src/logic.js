24622254f93ce3a3115c7d822ec8a4b4f7b7bbee
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-03-15 13:00:07 -0400

    Round values to the nearest whole number

--- src/logic.js
+++ src/logic.js
@@ -7,17 +7,17 @@ export function createCurrWeatherObj(dataObj){
     const date = format(dataObj.location.localtime, 'E, MMM. do, yyyy');
     const lastUpdatedDate = dataObj.current.last_updated;
     const lastUpdatedTime = format(dataObj.current.last_updated, 'p');
-    const tempF = dataObj.current.temp_f;
-    const tempC = dataObj.current.temp_c;
+    const tempF = Math.round(dataObj.current.temp_f);
+    const tempC = Math.round(dataObj.current.temp_c);
     const conditionText = dataObj.current.condition.text;
     const conditionIcon = dataObj.current.condition.icon;
-    const windMph = dataObj.current.wind_mph;
-    const windKph = dataObj.current.wind_kph;
-    const precipInches = dataObj.current.precip_in;
-    const precipMm = dataObj.current.precip_mm;
+    const windMph = Math.round(dataObj.current.wind_mph);
+    const windKph = Math.round(dataObj.current.wind_kph);
+    const precipInches = Math.round(dataObj.current.precip_in);
+    const precipMm = Math.round(dataObj.current.precip_mm);
     const {humidity} = dataObj.current;
-    const tempFeelF = dataObj.current.feelslike_f;
-    const tempFeelC = dataObj.current.feelslike_c;
+    const tempFeelF = Math.round(dataObj.current.feelslike_f);
+    const tempFeelC = Math.round(dataObj.current.feelslike_c);
     
     return { locationName, date, lastUpdatedDate, lastUpdatedTime, tempF, tempC, conditionText, conditionIcon, windMph, windKph, precipInches, precipMm, humidity, tempFeelF, tempFeelC };
 }
@@ -27,10 +27,10 @@ export function createDailyWeatherObj(dataObj, dayIncrementor){
 
     const parsedDate = parseISO(dataObj.forecast.forecastday[dayIncrementor].date);
     const conditionIcon = dataObj.forecast.forecastday[dayIncrementor].day.condition.icon;
-    const maxTempF = dataObj.forecast.forecastday[dayIncrementor].day.maxtemp_f;
-    const maxTempC = dataObj.forecast.forecastday[dayIncrementor].day.maxtemp_c;
-    const minTempF = dataObj.forecast.forecastday[dayIncrementor].day.mintemp_f;
-    const minTempC = dataObj.forecast.forecastday[dayIncrementor].day.mintemp_c;
+    const maxTempF = Math.round(dataObj.forecast.forecastday[dayIncrementor].day.maxtemp_f);
+    const maxTempC = Math.round(dataObj.forecast.forecastday[dayIncrementor].day.maxtemp_c);
+    const minTempF = Math.round(dataObj.forecast.forecastday[dayIncrementor].day.mintemp_f);
+    const minTempC = Math.round(dataObj.forecast.forecastday[dayIncrementor].day.mintemp_c);
     let dayOfWeek = format(parsedDate, 'cccc');
 
     if(isToday(parsedDate)){
@@ -45,8 +45,8 @@ export function createHourlyWeatherObj(dataObj, dailyIncrementor, hourIncremento
 
     const hourOfDay = format(dataObj.forecast.forecastday[dailyIncrementor].hour[hourIncrementor].time, "ha");
     const conditionIcon = dataObj.forecast.forecastday[dailyIncrementor].hour[hourIncrementor].condition.icon;
-    const tempF = dataObj.forecast.forecastday[dailyIncrementor].hour[hourIncrementor].temp_f;
-    const tempC = dataObj.forecast.forecastday[dailyIncrementor].hour[hourIncrementor].temp_c;
+    const tempF = Math.round(dataObj.forecast.forecastday[dailyIncrementor].hour[hourIncrementor].temp_f);
+    const tempC = Math.round(dataObj.forecast.forecastday[dailyIncrementor].hour[hourIncrementor].temp_c);
 
     return { hourOfDay, conditionIcon, tempF, tempC };
 }
