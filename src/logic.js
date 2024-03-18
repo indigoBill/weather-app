932ef6d4dc2a715eeb3f5dd7ea0f5924266022cb
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-03-17 21:50:46 -0400

    Add function to import required icons

--- src/logic.js
+++ src/logic.js
@@ -1,5 +1,16 @@
 import { format, parseISO, isToday } from 'date-fns';
 
+function getImagePath(conditionIcon){
+    const image = require.context('./assets/', true, /\.png$/);
+
+    // THE conditionIcon PATH IS FROM THE WEATHER API SO TO CONNECT THAT PATH TO THE PATH IN THE LOCAL ASSETS FOLDER THE ORIGINAL PATH NEEDS TO BE SLICED 
+    // AFTER THE .com AND APPENDED TO THE PATH WITHIN THE LOCAL DIRECTORY
+    const sliceStart = conditionIcon.indexOf('m') + 2;
+    const pathToAdd = conditionIcon.slice(sliceStart);
+
+    return image(`./${pathToAdd}`);
+}
+
 export function createCurrWeatherObj(dataObj){
     if(!dataObj) return;
 
@@ -10,7 +21,7 @@ export function createCurrWeatherObj(dataObj){
     const tempF = Math.round(dataObj.current.temp_f);
     const tempC = Math.round(dataObj.current.temp_c);
     const conditionText = dataObj.current.condition.text;
-    const conditionIcon = dataObj.current.condition.icon;
+    const conditionIcon = getImagePath(dataObj.current.condition.icon);
     const windMph = Math.round(dataObj.current.wind_mph);
     const windKph = Math.round(dataObj.current.wind_kph);
     const precipInches = Math.round(dataObj.current.precip_in);
@@ -26,7 +37,7 @@ export function createDailyWeatherObj(dataObj, dayIncrementor){
     if(!dataObj) return;
 
     const parsedDate = parseISO(dataObj.forecast.forecastday[dayIncrementor].date);
-    const conditionIcon = dataObj.forecast.forecastday[dayIncrementor].day.condition.icon;
+    const conditionIcon = getImagePath(dataObj.forecast.forecastday[dayIncrementor].day.condition.icon);
     const maxTempF = Math.round(dataObj.forecast.forecastday[dayIncrementor].day.maxtemp_f);
     const maxTempC = Math.round(dataObj.forecast.forecastday[dayIncrementor].day.maxtemp_c);
     const minTempF = Math.round(dataObj.forecast.forecastday[dayIncrementor].day.mintemp_f);
@@ -44,7 +55,7 @@ export function createHourlyWeatherObj(dataObj, dailyIncrementor, hourIncremento
     if(!dataObj) return;
 
     const hourOfDay = format(dataObj.forecast.forecastday[dailyIncrementor].hour[hourIncrementor].time, "ha");
-    const conditionIcon = dataObj.forecast.forecastday[dailyIncrementor].hour[hourIncrementor].condition.icon;
+    const conditionIcon = getImagePath(dataObj.forecast.forecastday[dailyIncrementor].hour[hourIncrementor].condition.icon);
     const tempF = Math.round(dataObj.forecast.forecastday[dailyIncrementor].hour[hourIncrementor].temp_f);
     const tempC = Math.round(dataObj.forecast.forecastday[dailyIncrementor].hour[hourIncrementor].temp_c);
 
